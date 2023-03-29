f03303aeb45681d0347c56a517e5300eb621ab80
ClaudiaYao <claudia.yao@foodpanda.com>
2023-03-30 01:09:37 +0800

    update these sample generation files by copying them from subscription-service, just to keep consistent

--- cmd/GenerateData/generateData.go
+++ cmd/GenerateData/generateData.go
@@ -14,6 +14,15 @@ import (
 	"github.com/lithammer/shortuuid"
 )
 
+type allIDInfo struct {
+	categories           []string
+	restaurants          []string
+	dishes               []string
+	playlists            []string
+	playlistDishRelation map[string][]string
+	subscriptions        []string
+}
+
 // open address file and store them in a slice of structs
 type address struct {
 	unit_number   string
@@ -25,12 +34,314 @@ type address struct {
 func main() {
 	categoryCodes := generateCategory()
 	restaurantIDs := generateRestaurant(categoryCodes)
-	// since Dish table has a foreign reference key to RestaurantID, so we
-	// pass restaurantIDs to the dish generation function
+	// // since Dish table has a foreign reference key to RestaurantID, so we
+	// // pass restaurantIDs to the dish generation function
 	dishIDs := generateDish(restaurantIDs)
 	playlistIDs := generatePlaylist(categoryCodes)
-	generatePlaylistDishRelation(playlistIDs, dishIDs)
 
+	playlistDishRelation := generatePlaylistDishRelation(playlistIDs, dishIDs)
+	// // when generating subscription data, subscriptionDish file and DishDelivery file is gnerated
+	// // together because they are closely related and need to refer to the data from playlist
+	// // service
+	subscriptionIDs := generateSubscription(playlistIDs, playlistDishRelation, dishIDs)
+
+	keepData(categoryCodes, restaurantIDs, dishIDs, playlistIDs, playlistDishRelation, subscriptionIDs)
+
+}
+
+func keepData(categories []string, restaurantsIDs []string, dishIDs []string, playlistIDs []string, playlistDishRelations map[string][]string, subscriptionIDs []string) {
+	write_f, err := os.Create("Generated/all_IDs.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	// remember to close the file at the end of the method
+	defer write_f.Close()
+
+	data := strings.Join(categories, "|") + "\n"
+	data += strings.Join(restaurantsIDs, "|") + "\n"
+	data += strings.Join(dishIDs, "|") + "\n"
+	data += strings.Join(playlistIDs, "|") + "\n"
+	data += strings.Join(subscriptionIDs, "|") + "\n"
+
+	_, err = write_f.WriteString(data)
+	if err != nil {
+		log.Fatal("error occurs when writing to file:", err)
+	}
+
+	write_rel, err := os.Create("Generated/playlistDishRel.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	// remember to close the file at the end of the method
+	defer write_rel.Close()
+	data = ""
+	for key, value := range playlistDishRelations {
+		data += key + "|" + strings.Join(value, "|") + "\n"
+	}
+	_, err = write_rel.WriteString(data)
+	if err != nil {
+		log.Fatal("error occurs when writing to file:", err)
+	}
+
+}
+
+func extractIDs() (allIDInfo, error) {
+	readFile, err := os.Open("Generated/all_IDs.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	// remember to close the file at the end of the program
+	defer readFile.Close()
+
+	fileScanner := bufio.NewScanner(readFile)
+
+	fileScanner.Split(bufio.ScanLines)
+
+	i := 0
+	ids := allIDInfo{}
+
+	for fileScanner.Scan() {
+		items := strings.Split(fileScanner.Text(), "|")
+
+		switch i {
+		case 0:
+			ids.categories = items
+		case 1:
+			ids.restaurants = items
+		case 2:
+			ids.dishes = items
+		case 3:
+			ids.playlists = items
+		case 4:
+			ids.subscriptions = items
+		}
+		i += 1
+
+	}
+
+	readFileRel, err := os.Open("Generated/playlistDishRel.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	// remember to close the file at the end of the program
+	defer readFileRel.Close()
+
+	fileScanner = bufio.NewScanner(readFileRel)
+
+	fileScanner.Split(bufio.ScanLines)
+
+	playlistDishRel := map[string][]string{}
+
+	for fileScanner.Scan() {
+		items := strings.Split(fileScanner.Text(), "|")
+		playlistDishRel[items[0]] = items[1:]
+	}
+
+	ids.playlistDishRelation = playlistDishRel
+	return ids, err
+
+}
+
+func generateDishDeliveryRecords(subscriptionDishID string, ExpectedTime time.Time) {
+	write_f, err := os.OpenFile("Generated/DishesDelivery.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// remember to close the file at the end of the program
+	defer write_f.Close()
+
+	statusChoice := []string{"Completed", "Cancelled", "Pending"}
+	id := "DD" + shortuuid.New()
+	status := statusChoice[rand.Intn(len(statusChoice))]
+	deliveryTime := ExpectedTime.Add(time.Minute * time.Duration(rand.Intn(500)))
+	note := "on time"
+
+	new_text := ""
+
+	// Parse the data record
+	if status == "Completed" {
+		new_text = id + "|" + subscriptionDishID + "|" + status + "|" +
+			ExpectedTime.Format(time.RFC3339) + "|" +
+			deliveryTime.Format(time.RFC3339) + "|" + note
+	} else {
+		new_text = id + "|" + subscriptionDishID + "|" + status + "|" +
+			ExpectedTime.Format(time.RFC3339) + "|" +
+			"\\N" + "|" + "not deliver yet"
+	}
+	_, err = write_f.WriteString(new_text + "\n")
+	if err != nil {
+		log.Fatal("error occurs when writing to file:", err)
+	}
+
+}
+
+func nextDelivery(frequency string, thisDelivery time.Time) time.Time {
+	if frequency == "daily" {
+		return thisDelivery.AddDate(0, 0, 1)
+	} else if frequency == "weekly" {
+		return thisDelivery.AddDate(0, 0, 7)
+
+	} else if frequency == "monthly" {
+		return thisDelivery.AddDate(0, 1, 0)
+	}
+	return thisDelivery.AddDate(0, 0, 1)
+}
+
+func generateSubscriptionDishes(subscriptionID string, startDate time.Time, endDate time.Time, frequency string, dishIDs []string) []string {
+	write_f, err := os.OpenFile("Generated/subscriptionDishes.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// remember to close the file at the end of the program
+	defer write_f.Close()
+
+	// customized means the user chooses own playlist
+
+	subscriptionDishIDs := []string{}
+
+	noteChoice := []string{
+		"perferendis volupt",
+		"veniam ut exc",
+		"fugit, amet",
+		"nulla consequatur",
+	}
+	for _, dishID := range dishIDs {
+		id := "SDish" + shortuuid.New()
+		scheduleTime := startDate.AddDate(0, 0, rand.Intn(3))
+		note := noteChoice[rand.Intn(len(noteChoice))]
+
+		var dishOptions = make([][]string, 2)
+		if rand.Intn(2) < 1 {
+			dishOptions = [][]string{
+				{"Mentaico Source", "Yes"},
+				{"Wasabi", "No"},
+			}
+		} else {
+			dishOptions = [][]string{
+				{"More source", "No"},
+				{"Pepper and Chili", "No"},
+			}
+		}
+
+		optionsB, err := json.Marshal(dishOptions)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		new_text := id + "|" + dishID + "|" + subscriptionID + "|" +
+			scheduleTime.Format(time.RFC3339) +
+			"|" + frequency + "|" + string(optionsB) + "|" + note
+
+		nextTime := scheduleTime
+		for nextTime.Before(endDate) {
+			generateDishDeliveryRecords(id, nextTime)
+			nextTime = nextDelivery(frequency, nextTime)
+		}
+
+		_, err = write_f.WriteString(new_text + "\n")
+		if err != nil {
+			log.Fatal("error occurs when writing to file:", err)
+		}
+		subscriptionDishIDs = append(subscriptionDishIDs, id)
+
+	}
+
+	return subscriptionDishIDs
+
+}
+
+func generateSubscription(playlistIDs []string, playlistDishRelations map[string][]string, dishIDs []string) []string {
+
+	write_f, err := os.Create("Generated/subscription.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	// remember to close the file at the end of the method
+	defer write_f.Close()
+
+	write_dishes_f, err := os.Create("Generated/subscriptionDishes.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	// remember to close the file at the end of the method
+	write_dishes_f.Close()
+
+	write_dishes_delivery, err := os.Create("Generated/dishesDelivery.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	// remember to close the file at the end of the method
+	write_dishes_delivery.Close()
+
+	statusInfo := []string{
+		"Active",
+		"Cancelled",
+		"Pending",
+		"Expired",
+	}
+
+	frequencyChoices := []string{
+		"Daily",
+		"Weekly",
+		"Monthly",
+	}
+
+	customizedChoice := []string{
+		"true",
+		"false",
+	}
+
+	subscriptionIDs := []string{}
+
+	names := []string{"Tody Liang", "Jone Tew", "James"}
+
+	// Get each playlist id and then form the table content
+	for _, playlistID := range playlistIDs {
+		chosen := rand.Intn(10)
+		// for each playlistID, generate some subscriptions
+		for i := 0; i < chosen; i++ {
+			id := "Sub" + shortuuid.New()
+			userID := "user" + strconv.Itoa(rand.Intn(20))
+			customized := customizedChoice[rand.Intn(len(customizedChoice))]
+			frequency := frequencyChoices[rand.Intn(len(frequencyChoices))]
+			status := statusInfo[rand.Intn(len(statusInfo))]
+			startDate := time.Now().AddDate(0, 0, -rand.Intn(15))
+			endDate := startDate.AddDate(0, 0, 7*rand.Intn(3))
+			receiverName := names[rand.Intn(len(names))]
+			receiverContact := strconv.Itoa(80000000 + rand.Intn(200000))
+
+			new_text := ""
+			if customized == "false" {
+				new_text = id + "|" + userID + "|" + playlistID + "|" +
+					customized + "|" + status + "|" + frequency + "|" + startDate.Format(time.RFC3339) +
+					"|" + endDate.Format(time.RFC3339) + "|" + receiverName +
+					"|" + receiverContact
+
+				generateSubscriptionDishes(id, startDate, endDate, frequency, playlistDishRelations[playlistID])
+
+			} else {
+				new_text = id + "|" + userID + "|" + "(empty)" + "|" +
+					customized + "|" + status + "|" + frequency + "|" + startDate.Format(time.RFC3339) +
+					"|" + endDate.Format(time.RFC3339) + "|" + receiverName +
+					"|" + receiverContact
+
+				randomDishes := []string{}
+				for i := 0; i < rand.Intn(5); i++ {
+					randomDishes = append(randomDishes, dishIDs[i])
+				}
+
+				generateSubscriptionDishes(id, startDate, endDate, frequency, randomDishes)
+			}
+
+			_, err := write_f.WriteString(new_text + "\n")
+			if err != nil {
+				log.Fatal("error occurs when writing to file:", err)
+			}
+			subscriptionIDs = append(subscriptionIDs, id)
+		}
+
+	}
+	return subscriptionIDs
 }
 
 func generatePlaylist(categoryCodes []string) []string {
@@ -71,7 +382,7 @@ func generatePlaylist(categoryCodes []string) []string {
 		categoryCode := categoryCodes[rand.Intn(categoryNumber)]
 		dietary := dietaryInfo[rand.Intn(dietaryNumber)]
 
-		startDateRandom := time.Now().AddDate(0, 0, rand.Intn(60)-30)
+		startDateRandom := time.Now().AddDate(0, 0, rand.Intn(10)-10)
 		startDate := startDateRandom.Format("2006-01-02")
 		endDateRandom := startDateRandom.AddDate(0, 2, 15)
 		end_date := endDateRandom.Format("2006-01-02")
@@ -106,7 +417,7 @@ func generatePlaylist(categoryCodes []string) []string {
 
 }
 
-func generatePlaylistDishRelation(playlistIDs []string, dishIDs []string) {
+func generatePlaylistDishRelation(playlistIDs []string, dishIDs []string) map[string][]string {
 
 	write_f, err := os.Create("Generated/playlist_dish.txt")
 	if err != nil {
@@ -122,13 +433,17 @@ func generatePlaylistDishRelation(playlistIDs []string, dishIDs []string) {
 		fmt.Println("too less dishes in the database.")
 	}
 
+	playlistDishRelation := map[string][]string{}
+
 	// Get each playlist name and then form the table content
 	for _, playlistID := range playlistIDs {
 
 		// do something with a line
 
-		dishNum := 2 + rand.Intn(3)
+		dishNum := 4 + rand.Intn(6)
 		chosen := 0
+
+		dishes := []string{}
 
 		for chosen < dishNum {
 			id := "PD" + shortuuid.New()
@@ -139,12 +454,14 @@ func generatePlaylistDishRelation(playlistIDs []string, dishIDs []string) {
 			if err != nil {
 				log.Fatal("error occurs when writing to file:", err)
 			}
-
+			dishes = append(dishes, dishID)
 			chosen += 1
 
 		}
+		playlistDishRelation[playlistID] = dishes
 
 	}
+	return playlistDishRelation
 }
 
 func generateDish(restaurantIDs []string) []string {
